499de1e22a3265136549d7f2597abc1a1cec88ab
Andres Rivero <andres19rivero@gmail.com>
2024-08-03 17:39:29 -0400

    format & add constructor to Ingredient class

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -5,11 +5,17 @@ import lombok.Data;
 @Data
 public class Ingredient {
 
-    private final String id;
-    private final String name;
-    private final Type type;
+  private final String id;
+  private final String name;
+  private final Type type;
 
-    public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
+  public enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
+
+  public Ingredient(String id, String name, Type type) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+  }
 }
